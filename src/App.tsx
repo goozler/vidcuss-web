5f7a4beb1d0273588e863fbff3bffdf9903643d7
Krutov Alexander <goozler@mail.ru>
2019-09-12 02:52:50 +0400

    chore: bootstraping

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Box, Flex, Text } from "rebass";
+import Video from "./components/Video";
 
 const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Box m={3}>
+      <Flex mx={-2}>
+        <Box width={1 / 2} px={2}>
+          <Video />
+        </Box>
+        <Box width={1 / 2} px={2}>
+          <Text p={1} color="background" bg="primary">
+            Какие-то комментрии тут
+          </Text>
+          <textarea />
+        </Box>
+      </Flex>
+    </Box>
   );
-}
+};
 
 export default App;
